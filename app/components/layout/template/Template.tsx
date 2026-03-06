918a5f1f7d88777da024f71c89231947a9f4fd0b
SiddiquiSaab04 <devlaiba04@gmail.com>
2026-03-06 23:23:17 +0500

    feat: initial commit

--- app/components/layout/template/Template.tsx
+++ app/components/layout/template/Template.tsx
@@ -1,4 +1,3 @@
-"use client"
 
 export default function Template({ children , styleClass }: { children?: React.ReactNode , styleClass?:string}) {
   return <div className={`p-6 ${styleClass} `}>{children}</div>;
