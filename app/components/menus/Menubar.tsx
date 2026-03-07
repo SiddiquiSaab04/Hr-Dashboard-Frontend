22bacf591c93f77642afde9951003678c0577250
SiddiquiSaab04 <devlaiba04@gmail.com>
2026-03-07 12:05:46 +0500

    add icons in menu

--- app/components/menus/Menubar.tsx
+++ app/components/menus/Menubar.tsx
@@ -5,25 +5,22 @@ import { usePathname } from "next/navigation";
 export default function Menubar() {
   const pathname = usePathname();
   return (
-
-      <div
-        className={`h-dvh w-64 bg-gray-100 dark:bg-gray-900 border-r border-gray-300 dark:border-gray-700 transform transition-transform duration-300 md:translate-x-0`}
-      >
-        <div className="p-4 font-bold text-xl">My Sidebar</div>
-        <nav className="flex flex-col gap-2 p-4">
-         {
-          Menulist.map((menu)=>(
-            <Link
-              key={menu.url}
-              href={menu.url}
-              className={`${pathname === menu.url ? "bg-gray-200 dark:bg-gray-800" : "hover:bg-gray-200 dark:hover:bg-gray-800"} px-3 py-2 rounded`}
-            >
-              {menu.name}
-            </Link>
-          ))
-         }
-        </nav>
-      </div>
-  
+    <div
+      className={`h-dvh w-64 bg-gray-100 dark:bg-gray-900 border-r border-gray-300 dark:border-gray-700 transform transition-transform duration-300 md:translate-x-0`}
+    >
+      <div className="p-4 font-bold text-xl">My Sidebar</div>
+      <nav className="flex flex-col gap-2 p-4">
+        {Menulist.map((menu) => (
+          <Link
+            key={menu.url}
+            href={menu.url}
+            className={` flex gap-x-3 items-center ${pathname === menu.url ? "bg-gray-200 dark:bg-gray-800" : "hover:bg-gray-200 dark:hover:bg-gray-800"} px-3 py-2 rounded`}
+          >
+            <p>{menu.icon}</p>
+            <p>{menu.name}</p>
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
--- app/components/menus/menu-list/Menulist.tsx
+++ app/components/menus/menu-list/Menulist.tsx
@@ -1,11 +1,29 @@
+import {
+  Building,
+  LayoutDashboardIcon,
+  UserMinusIcon,
+  UsersIcon,
+} from "lucide-react";
 
 export const Menulist = [
-    {
-        name: "Dashboard",
-        url: "/pages/home"
-    },
-    {
-        name: "Profile",
-        url: "/profile"
-    }
-]
+  {
+    name: "Dashboard",
+    url: "/pages/home",
+    icon: <LayoutDashboardIcon size={18} />,
+  },
+  {
+    name: "Attendance",
+    url: "/pages/leave",
+    icon: <UserMinusIcon size={18} />,
+  },
+  {
+    name: "Departments",
+    url: "/pages/departments",
+    icon: <Building size={18} />,
+  },
+  {
+    name: "Employees",
+    url: "/pages/employees",
+    icon: <UsersIcon size={18} />,
+  },
+];
